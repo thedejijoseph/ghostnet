f1a1a22a8f9c6d3cf36e5591ac8422ff81eea6ba
Deji <wrecodde@gmail.com>
2019-04-13 01:08:35 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
-import sockets
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('', 80))
+
+server.listen(3)
+conn, addr = server.accept()
+print(addr)
+server.close()
